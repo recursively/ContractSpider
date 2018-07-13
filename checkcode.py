4bed17a678e6a4b827978b673accb284b56c19e1
recursively <reindeer.zyh@gmail.com>
2018-07-13 20:47:50 +0800

    write result to dirs

--- checkcode.py
+++ checkcode.py
@@ -7,22 +7,28 @@ class MongodbConn(object):
 
     def __init__(self):
         self.connection = pymongo.MongoClient("mongodb://%s:%s@192.168.10.220" % ('root', 'ATWtGZhsP4FLTYUf'), port=22223)
+        self.count = 1
 
     def run(self):
         db = self.connection['SC']
         sc = db.Sourcecode
         cursor = sc.find()
+        total = cursor.count()
         if not os.path.exists('contracts'):
             os.mkdir('contracts')
         for document in cursor:
+            print(str(self.count) + '/' + str(total))
             addr = document['Address']
             sourcecode = document['Sourcecode']
             if not os.path.exists('./contracts/' + addr):
                 os.mkdir('./contracts/' + addr)
             with open('./contracts/' + addr + '/' + addr + '.sol', 'w') as f:
                 f.write(sourcecode)
-            # files = subprocess.Popen('ls', shell=True)
-            # subprocess.call('manticore ' + '-h', shell=True)
+            os.chdir('./contracts/' + addr)
+            try:
+                subprocess.call('manticore ' + addr + '.sol', shell=True)
+            except Exception as e:
+                print(e)
 
 
 if __name__ == '__main__':
