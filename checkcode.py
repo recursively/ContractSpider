cadacf6e63daae71da2120e472dda666469da42f
recursively <reindeer.zyh@gmail.com>
2018-07-14 11:31:10 +0800

    change the timeout

--- checkcode.py
+++ checkcode.py
@@ -26,7 +26,7 @@ class MongodbConn(object):
             with open(addr + '.sol', 'w') as f:
                 f.write(sourcecode)
             try:
-                subprocess.call('exec manticore ' + addr + '.sol', shell=True, timeout=120)
+                subprocess.call('exec manticore ' + addr + '.sol', shell=True, timeout=300)
             except Exception as e:
                 print(e)
             self.count += 1
