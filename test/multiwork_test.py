4b6f024607b4a62fbef55d148a209be3637e53c2
recursively <reindeer.zyh@gmail.com>
2018-07-19 12:04:16 +0800

    test result writing

--- test/multiwork_test.py
+++ test/multiwork_test.py
@@ -8,3 +8,9 @@ def test_multiwork():
     manager = Manager()
     blocklist = manager.list(range(blockstart, blockend))
     assert multi_thread_scrape(blocklist=blocklist, thread=20) == 'finished'
+
+
+def test_result():
+    with open('contract_code.txt', 'r') as f:
+        content = f.read()
+    assert 'AmIOnTheFork' in content
