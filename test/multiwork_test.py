3055cf4688c5899c2645e0c6a84f18aff97a5068
recursively <reindeer.zyh@gmail.com>
2018-07-19 11:23:49 +0800

    change the import method

--- test/multiwork_test.py
+++ test/multiwork_test.py
@@ -1,12 +1,10 @@
 from multiprocessing import Manager
-import os
-from ..ETHspider import multi_thread_scrape
+from ETHspider import multi_thread_scrape
 
 blockstart, blockend = 2103333, 2103338
 
 
 def test_multiwork():
-    print(os.getcwd())
     manager = Manager()
     blocklist = manager.list(range(blockstart, blockend))
     assert multi_thread_scrape(blocklist=blocklist, thread=20) == 'finished'
